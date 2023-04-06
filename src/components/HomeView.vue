e36b5d9260deb90d70ae5b8bec3f818a4c5115ae
scobca <scobca18@yandex.ru>
2023-04-07 02:00:49 +0300

    hovering for card on HomeView, and more styles for main page

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -7,14 +7,15 @@
         <section class="welcome_container container">
             <div class="welcome_content">
                 <header class="welcome_header">
-                    <h2> Добро пожаловать!</h2>
+                    <h2> Добро пожаловать! </h2>
                 </header>
 
                 <div class="welcome_footer">
                     <p class="fContent_head"> Коротко о нас: </p>
-                    <p class="footer_content">
-                        <br> мы являемся академовцами из академии цифровых технологий, вообще ковалёв красавчик что тут еще сказать
-                        АааААааААаААааАааАааАаААА аАааАаАааАооаОАОоао АООАОООАоооаоаоаоо
+                    <p class="fContent">
+                        Мы являемся официальным дистрибьютером компании NSP в Санкт-Петербурге.
+                        <br> <br> Директор нашего офиса, наш лидер
+                        - Фокина Ольга Сергеевна!
                     </p>
                 </div>
             </div>
@@ -43,25 +44,68 @@ export default {
         margin-left: 2.5vw;
     }
 
-    .welcome_container {
+    .welcome_container, .footer_content {
         width: auto;
         max-width: 350px;
     }
 
-    .welcome_content, .welcome_footer{
-        height: 100vh;
+    .welcome_content{
+        height: auto;
+        scale: 100%;
+        transition-duration: 0.5s;
     }
-    
+
+    .welcome_header {
+        font-family: Comfortaa, cursive;
+        height: auto;
+
+        background-image: url("../../src/assets/background/bg-grad-1.jpg") ;
+        background-size: 100%;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+
+        text-align: center;
+        vertical-align: center;
+    }
+
+    .welcome_header > h2 {
+        padding: 8px;
+        letter-spacing: 1px;
+    }
+
     .welcome_footer {
         padding: 16px;
+        margin: auto;
         text-align: left;
     }
 
     .fContent_head {
         text-align: center;
+        font-size: 1.6rem;
+
         padding: 0;
-        font-size: 1.7rem;
-        margin-bottom: -2.5vh;
+        margin-top: -1vh;
+        margin-bottom: 1vh;
+
+        width: auto;
+        border-bottom: 1px solid rgba(223, 214, 229, 0);
+    }
+
+    .fContent {
+        text-indent: 0;
+        padding: 0 5px 0 10px;
+    }
+
+    .welcome_content:hover {
+        scale: 110%;
+        transition-duration: 0.5s;
+    }
+
+    .welcome_content:hover .fContent_head {
+        border-bottom: white 1px solid;
+        box-sizing: border-box;
+
+        transition: all 1s linear;
     }
 
     @media (prefers-color-scheme: dark) {
@@ -70,23 +114,13 @@ export default {
             border-radius: 10px;
         }
 
-        .welcome_header {
-            font-family: Comfortaa, cursive;
-            height: auto;
-
-            background-image: url("../../src/assets/background/bg-grad-1.jpg") ;
-            background-size: 100%;
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
-
-            text-align: center;
-            vertical-align: center;
+        .welcome_header > h2 {
+            color: var(--vt-c-white);
         }
 
-        .welcome_header > h2 {
-            padding: 8px;
-            letter-spacing: 2px;
+        .fContent_head {
             color: var(--vt-c-white-mute);
+            font-weight: 100;
         }
     }
 </style>
