e2748ea70e4442c3c8ff40c27cf93a21a735c2df
scobca <scobca18@yandex.ru>
2023-04-07 23:50:06 +0300

    block about director, animations and aos-delay for all main blocks. Bug need fixing: transition color of blocks content black -> white

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,8 +3,12 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
 
-    <section class="welcome" data-aos="fade-down" data-aos-duration="1000">
-        <section class="welcome_container container">
+    <section class="welcome">
+        <section class="welcome_container container"
+                 data-aos="fade-down"
+                 data-aos-duration="1500"
+                 data-aos-delay="350">
+
             <div class="welcome_content">
                 <header class="welcome_header">
                     <h2> Добро пожаловать! </h2>
@@ -19,12 +23,25 @@
                     </p>
                 </div>
             </div>
+        </section>
+
+        <section class="director_container container"
+                 data-aos="fade-down"
+                 data-aos-duration="1500"
+                 data-aos-delay="550">
 
             <div class="director_content">
-                <header class="director_header"> <h2> О нашем директоре: </h2> </header>
-                <p class="fContent_head"></p>
-                <p class="fContent">Нащ директор - Фокина Ольга Сергеевна, основное образование - инженер, прошла курсы
-                нутрициолгоии.</p>
+                <header class="director_header">
+                    <h2> О нашем директоре </h2>
+                </header>
+
+                <footer class="director_footer">
+                    <p class="fContent_head">Иванов иван Иванович</p>
+                    <p class="fContent">
+                        Образование: инженер-экономист
+                        Прошел курсы нутрициологии
+                    </p>
+                </footer>
             </div>
         </section>
     </section>
@@ -48,70 +65,49 @@ export default {
     .welcome {
         display: flex;
         flex-direction: row;
-        justify-content: left;
-        margin-top: 8vh;
-        margin-left: 2.5vw;
+        justify-content: space-evenly;
+        margin: 8vh 2.5vw 10vh;
     }
 
-    .welcome_container {
+    .welcome_container, .director_container {
         display: flex;
-
         width: auto;
+        padding-left: .5vw;
+        padding-right: .5vw;
     }
 
-    .welcome_content{
+    .welcome_content, .director_content{
         height: auto;
+        width: auto;
+        max-width: 350px;
         scale: 100%;
         transition-duration: 0.5s;
-        width: auto;
-        max-width: 350px;
     }
 
-    .director_content {
+    .welcome_header, .director_header {
+        font-family: Comfortaa, cursive;
         height: auto;
-        scale: 100%;
-        transition-duration: .5s;
-        width: auto;
-        max-width: 350px;
+
+        background-image: url("../../src/assets/background/bg-grad-1.jpg") ;
+        background-size: 100%;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+
+        text-align: center;
+        vertical-align: center;
     }
 
     .director_header {
-        font-family: Comfortaa, cursive;
-        height: auto;
-
-        background-image: url("../../src/assets/background/bg-grad-1.jpg") ;
+        background-image: url("../../src/assets/background/bg-grad-3.jpg") ;
         background-size: 100%;
-        border-top-left-radius: 10px;
-        border-top-right-radius: 10px;
-
-        text-align: center;
-        vertical-align: center;
     }
 
-    .director_header > h2 {
+    .welcome_header > h2, .director_header > h2 {
         padding: 8px;
         letter-spacing: 1px;
     }
 
-    .welcome_header {
-        font-family: Comfortaa, cursive;
-        height: auto;
-
-        background-image: url("../../src/assets/background/bg-grad-1.jpg") ;
-        background-size: 100%;
-        border-top-left-radius: 10px;
-        border-top-right-radius: 10px;
-
-        text-align: center;
-        vertical-align: center;
-    }
-
-    .welcome_header > h2 {
-        padding: 8px;
-        letter-spacing: 1px;
-    }
-
-    .welcome_footer {
+    .welcome_footer, .director_footer {
         padding: 16px;
         margin: auto;
         text-align: left;
@@ -134,12 +130,12 @@ export default {
         padding: 0 5px 0 10px;
     }
 
-    .welcome_content:hover {
-        scale: 110%;
+    .welcome_content:hover, .director_content:hover {
+        scale: 105%;
         transition-duration: 0.5s;
     }
 
-    .welcome_content:hover .fContent_head{
+    .welcome_content:hover .fContent_head, .director_content:hover .fContent_head{
         border-bottom: white 1px solid;
         box-sizing: border-box;
 
@@ -147,18 +143,18 @@ export default {
     }
 
     @media (prefers-color-scheme: dark) {
-        .welcome_content {
+        .welcome_content, .director_content {
             background-color: rgba(223, 214, 229, 0.15);
             border-radius: 10px;
         }
 
-        .welcome_header > h2 {
+        .welcome_header > h2, .director_header > h2 {
             color: var(--vt-c-white);
         }
 
         .fContent_head {
             color: var(--vt-c-white-mute);
-            font-weight: 100;
+            transition-duration: .1s;
         }
     }
 </style>
